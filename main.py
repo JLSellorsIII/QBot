2da432628767ad3b6e16103dad8f049d3c133d8c
JLSellorsIII <jeremy.sellors@alumni.ubc.ca>
2024-04-08 16:22:56 -0700

    Minor change

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 # bot.py
 import os
-
-import discord
 import random
-from dotenv import load_dotenv
+import discord
+import csv
+
 from discord.ext import commands
+from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+client = discord.Client(intents=intents)
 
 bot = commands.Bot(command_prefix='!')
+
+
+
+@bot.event
+async def on_message(message):
+    if (message.author == client.user):
+        return
+
+    if (message.author.id == 456165982143119361):
+        joe_responses = ["Shut up Joe", "Excellent Take Joe", "Log Off Joe"]
+        await message.channel.send(f"{random.choice(joe_responses)}")  # C
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+
 
 @bot.event
 async def on_member_join(member):
@@ -30,17 +48,38 @@ async def on_error(event, *args, **kwargs):
         else:
             raise
 
-@bot.command(name='roll')
-async def rollEx(ctx, number_of_dice: int, trick):
-    dice = [
-        str(random.choice(range(1,10 + 1)))
-        for _ in range(number_of_dice)
-        ]
-    await ctx.send(', '.join(dice))
 
-@bot.command(name='GenerateName')
-async def generateName(ctx):
+@bot.command(name='create_puppet')
+async def create_puppet(ctx, name, n):
+    userID = 0
+    for user in client.users:
+        if (user.name == name):
+            userID = user.id
+    corpus = create_corpus(userID, n)
+    writeCorpusToFile(corpus, name)
+
+async def writeCorpusToFile(corpus, name):
+    return
 
 
 
-bot.run(TOKEN)
+#creates an array made up of the contents of the given user's posts, up to n posts per channel, if n=0 then no limit
+async def create_corpus(userID: discord.User.id, n: int):
+    corpus = []
+    for guild in bot.guilds:
+        for channel in guild.text_channels:
+            corpus.append(scrape_channel(channel))
+    corpus = sum(corpus, [])
+    return corpus
+
+#collects a given user's posts from the given channel
+async def scrape_channel(channel: discord.TextChannel, userID: discord.User.id, n: int):
+    corpus = []
+    for message in channel.history(limit=n):
+        if message.author.id == userID:
+            corpus.append(message.content)
+    return corpus
+
+
+
+client.run(TOKEN)
